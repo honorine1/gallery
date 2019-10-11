4c52f2cf22fa75059e9236875cef2ac0f40d068d
honorine1 <uwurukundoh@gmail.com>
2019-10-11 10:32:20 +0200

    today_photos function is added

--- photos/models.py
+++ photos/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-
+import datetime as dt
 # Create your models here.
 
 class Editor(models.Model):
     username = models.CharField(max_length = 20)
     email = models.EmailField(max_length = 30)
+    pub_date = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.username
 
@@ -25,6 +26,15 @@ class Image(models.Model):
     image_description = models.CharField(max_length = 100)
     image_location = models.ForeignKey(Location)
     image_category = models.ForeignKey(Category)
+    image = models.ImageField(upload_to = 'images/')
+    pub_date = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.image_name
+
+
+    @classmethod
+    def todays_photos(cls):
+            today = dt.date.today()
+            photos = cls.objects.filter(pub_date__date = today)
+            return photos
 
