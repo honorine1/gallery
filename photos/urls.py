52ab937af6f7c71e578469bafe50f6253ecc0896
honorine1 <uwurukundoh@gmail.com>
2019-10-11 12:07:25 +0200

    search url is added

--- photos/urls.py
+++ photos/urls.py
@@ -6,7 +6,8 @@ from django.conf.urls.static import static
 urlpatterns=[
     url(r'^$',views.welcome,name = 'welcome'),
     url(r'^today/$',views.photos_of_day,name='photosToday'),
-    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_photos,name = 'pastPhotos')
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_photos,name = 'pastPhotos'),
+    url(r'^search/', views.search_images, name='search_images')
 ]
 if settings.DEBUG:
     urlpatterns+=static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
