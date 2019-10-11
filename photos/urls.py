f04f55c053bcf599d82339f9cd89bebf481f5b30
honorine1 <uwurukundoh@gmail.com>
2019-10-11 10:33:16 +0200

    debbugg setting for images is added

--- photos/urls.py
+++ photos/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
-    # url(r'^$',views.welcome,name = 'welcome'),
+    url(r'^$',views.welcome,name = 'welcome'),
     url(r'^today/$',views.photos_of_day,name='photosToday'),
     url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_photos,name = 'pastPhotos')
 ]
-
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
 
