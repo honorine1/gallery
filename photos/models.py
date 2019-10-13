d24e7cb807533d61e59e54042b4f29a484fffd28
honorine1 <uwurukundoh@gmail.com>
2019-10-14 01:05:35 +0200

    other content are  added

--- photos/models.py
+++ photos/models.py
@@ -6,42 +6,59 @@ class Editor(models.Model):
     username = models.CharField(max_length = 20)
     email = models.EmailField(max_length = 30)
     pub_date = models.DateTimeField(auto_now_add=True)
+
+
+    def save_editor(self):
+        self.save()
     def __str__(self):
         return self.username
 
+
 class Location(models.Model):
-    loc_name = models.CharField(max_length = 10)
+    location_name = models.CharField(max_length = 10)
+
+
+    def save_location(self):
+        self.save()
     def __str__(self):
-        return self.loc_name
+        return self.location_name
 
 class Category(models.Model):
-    cat_name = models.CharField(max_length = 40)
+    category_name = models.CharField(max_length = 40)
+
+    def save_category(self):
+        self.save()
     def __str__(self):
-        return self.cat_name
+        return self.category_name
 
-    @classmethod
-    def search_by_image_category(cls,search_term):
-        photos =cls.objects.filter(image_category__icontains=search_term)
-        return photos
-
+    
     
     
 
 class Image(models.Model):
+    # editor_name = models.CharField(max_length = 40)
     image_name = models.CharField(max_length = 40)
     image_description = models.CharField(max_length = 100)
-    image_location = models.ForeignKey(Location)
-    image_category = models.ForeignKey(Category)
+    image_location = models.ForeignKey(Location,db_column='location_name')
+    image_category = models.ForeignKey(Category,db_column='category_name')
     image = models.ImageField(upload_to = 'images/')
     pub_date = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.image_name
 
-
+    
+    def save_image(self):
+        self.save()
     @classmethod
     def todays_photos(cls):
             today = dt.date.today()
             photos = cls.objects.filter(pub_date__date = today)
             return photos
+
+    @classmethod
+    def search_by_category_name(cls,search_term):
+        photos =cls.objects.filter(image_category__category_name__contains=search_term)
+        return photos
+
 
   
--- virtual/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/wecode/Desktop/class-project/gallery-project/virtual/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
