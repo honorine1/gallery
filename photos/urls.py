9337cf17ae2d96abdf7c4b83db97e6b8bf890681
honorine1 <uwurukundoh@gmail.com>
2019-10-13 20:00:45 +0200

    url.py content is added

--- photos/urls.py
+++ photos/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
-    url(r'^$',views.welcome,name = 'welcome'),
+    url(r'^home',views.welcome,name = 'welcome'),
     url(r'^today/$',views.photos_of_day,name='photosToday'),
     url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_photos,name = 'pastPhotos'),
     url(r'^search/', views.search_images, name='search_images'),
