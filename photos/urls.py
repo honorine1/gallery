b6d63c80660c3667a4598c2c6872bdd7b4546d26
honorine1 <uwurukundoh@gmail.com>
2019-10-14 11:55:27 +0200

    index url replaced welcome url content is adde

--- photos/urls.py
+++ photos/urls.py
@@ -4,10 +4,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
-    url(r'^$',views.welcome,name = 'welcome'),
-    # url(r'^$',views.index,name = 'index'),
+    url(r'^$',views.index,name = 'index'),
     url(r'^today/$',views.photos_of_day,name='photosToday'),
-    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_photos,name = 'pastPhotos'),
+    # url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_photos,name = 'pastPhotos'),
     url(r'^search/', views.search_images, name='search_images'),
     url(r'^image/(\d+)',views.image,name ='image'),
 ]
